44d4c94c3537d9fb58e172d8291491c03052ca6a
Saul Uriel <sauluriel1402@gmail.com>
2021-08-21 22:25:20 -0600

    routes for the environments

--- farmENV/urls.py
+++ farmENV/urls.py
@@ -7,7 +7,9 @@ from django.conf.urls.static import static
 
 from users.views import users as users_views
 from users.views.login import UserLoginAPIView as login
+from environments.views.environments import EnvAPIView as environments
 from users.views.users import ProfileCompletionViewSet
+from environments.views.environments import EnvViewSet
 from environments.views import environments as environments_views
 
 #Django REST framework
@@ -15,13 +17,15 @@ from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register(r'profile', ProfileCompletionViewSet, basename = 'profile')
+router.register(r'env', EnvViewSet, basename = 'env')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', users_views.UserListView.as_view(), name='users'),
     path('users/signup/', users_views.signup, name='signup'),
-    path('users/verify/', users_views.account_verification, name='verify'),
+    path('users/verify/<token>/', users_views.account_verification, name='verify'),
     path('users/login/', login.as_view(), name='login'),
+    path('environments/', environments.as_view(), name='environments'),
     path('environments/create/',environments_views.perform_create, name='create'),
     path('', include(router.urls))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
